cefcb406bc2925f9be542c1cd80ef596467e2b0b
Pavlo Leinonen <leinonenko@gmail.com>
2023-10-04 16:06:03 +0300

    profile details function done

--- app/src/main/java/com/example/energywizeapp/MainActivity.kt
+++ app/src/main/java/com/example/energywizeapp/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.energywizeapp
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.energywizeapp.ui.navigation.mainNavigator.MainNavigator
 import com.example.energywizeapp.ui.theme.EnergyWizeAppTheme
 
@@ -26,3 +51,91 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+
+// I know it is messy, will be fixed later
+@Composable
+fun ProfileDetails() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .background(color = Color.LightGray)
+                .padding(8.dp)
+
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .align(Alignment.CenterVertically),
+            ) {
+                Text(text = "Hei, Jorma Sähköinen!", fontWeight = FontWeight.Bold)
+            }
+            Row(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(color = Color.LightGray)
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Column(
+                    modifier = Modifier.padding(8.dp),
+                    verticalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Row(
+                        modifier = Modifier,
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        Icon(imageVector = Icons.Default.Home, contentDescription = "Home")
+                        Spacer(modifier = Modifier.padding(6.dp))
+                        Text(text = "Valkyriankatu 12 C 36")
+                    }
+                    Spacer(modifier = Modifier.padding(4.dp))
+                    Row(
+                        modifier = Modifier,
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        Icon(imageVector = Icons.Default.Info, contentDescription = "Info")
+                        Spacer(modifier = Modifier.padding(6.dp))
+                        Text(text = "Sopimukset")
+                    }
+                    Spacer(modifier = Modifier.padding(4.dp))
+                    Row(
+                        modifier = Modifier,
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        Icon(imageVector = Icons.Default.Email, contentDescription = "Emal")
+                        Spacer(modifier = Modifier.padding(6.dp))
+                        Text(text = "sahko.jorma@gmail.com")
+                    }
+                    Spacer(modifier = Modifier.padding(4.dp))
+                    Row(
+                        modifier = Modifier,
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Plan")
+                        Spacer(modifier = Modifier.padding(6.dp))
+                        Text(text = "Omavoima super halpa")
+                    }
+                }
+            }
+        }
+    }
+}
+
+/*
+@Preview(showBackground = true)
+
+@Composable
+fun DefaultPreview() {
+    ProfileDetails()
+}
+ */
